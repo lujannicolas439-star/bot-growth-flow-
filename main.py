ffc97dd6cdf59b189b8ad8623b479af3fd57c14a
lujannicolas439-star <lujannicolas439@gmail.com>
2026-06-19 04:32:09 -0300

    Add Flask route to render index.html

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 import openai
 import os
 from flask import Flask, request
